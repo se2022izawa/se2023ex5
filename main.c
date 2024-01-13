8f3205d8db050362ee4db3c131aac1dc7fd87104
izawa <r202203397dmjindai.jp>
2024-01-14 01:58:32 +0900

    no3 version

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
     char str[] = "hello";
-    printf("%s\n", str);
+    size_t length = strlen(str);
+    printf("%s %zu\n", str, length);
     return 0;
 }
